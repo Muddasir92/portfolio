6529422379cdaef3af1d1b2304bb78828c79269e
muddasir <muddasirjaved92@gmail.com>
2024-07-30 03:59:00 +0500

    add CSS

--- src/components/folioBody/folioBody.jsx
+++ src/components/folioBody/folioBody.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "/src/components/folioBody/foliBody.css";
 import Profilepic from "/src/assets/profile-pic.png";
 import CVformat from "/src/assets/cvformat.pdf";
+import EmailPic from "/src/assets/mail.jpg";
 
 function folioBody() {
   return (
@@ -49,12 +50,12 @@ function folioBody() {
                     </a>
                   </li>
                   <li>
-                    <a href="">
+                    <a href="https://www.facebook.com/hafiz.muddasir4/">
                       <i className="fa fa-facebook"></i>
                     </a>
                   </li>
                   <li>
-                    <a href="">
+                    <a href="https://www.instagram.com/muddasir_javed9/">
                       <i className="fa fa-instagram"></i>
                     </a>
                   </li>
@@ -77,20 +78,22 @@ function folioBody() {
         </div>
       </section>
       {/* hello  */}
-      {/* About section */}
+      {/* ***********************    About section ***********************/}
 
-      <section className="container mt-80">
-        <h4 className="text-center about mt-4 " id="about">
-          About
-        </h4>
-        <hr className="block block-light" />
+      <section className="container mt-80 " id="about">
+        <div className="col-hr">
+          <h3 className="text-center  mt-4 " style={{ color: "#fafafa" }}>
+            About
+          </h3>
+          <hr className="block  block-light" />
+        </div>
         <div className="row">
           <div className=" col-sm-12 col-lg-6 shadow">
             <div className="card shadow m-3">
               <div className="card-body">
-                <h4 className="cart-title text-center">
-                  Hello This is M Muddasir
-                </h4>
+                <h5 className="cart-title text-center">
+                  Hello! This is M Muddasir Javed
+                </h5>
                 <p className="card-text">
                   During August 2022, I decided to take a leap of faith and
                   trained to become a Mern Stack developer.
@@ -100,8 +103,11 @@ function folioBody() {
                   than designing and developing cool websites.
                 </p>
                 <p>
-                  Got a cool project in mind or looking to hire a developer? Get
-                  in touch, and we can bring your idea to life.
+                  Got a cool project in mind or looking to hire a developer?
+                  <a href="#contact" className="get-in-touch">
+                    Get! in touch
+                  </a>{" "}
+                  and we can bring your idea to life.
                 </p>
               </div>
             </div>
@@ -109,7 +115,7 @@ function folioBody() {
           <div className=" col-sm-12 col-lg-6 shadow">
             <div className="card shadow m-3 about-height">
               <div className="card-body">
-                <h4 className="card-title text-center">Personal Details</h4>
+                <h5 className="card-title text-center">Personal Details</h5>
                 <div className="row">
                   <div className="col-sm-5">
                     <p>
@@ -146,16 +152,20 @@ function folioBody() {
         </div>
       </section>
 
-      {/* skills */}
-      <section className="container ">
-        <h4 className="pt-4 text-center">Skills</h4>
-        <hr className="block" />
+      {/* *********************             skills                 ***************************** */}
+      <section className="container " id="skills">
+        <div className="col-hr">
+          <h4 className="pt-4 text-center">Skills</h4>
+          <hr className="block " />
+        </div>
         <div className="row">
           {/* Tech skills col left */}
 
           <div className="col-sm-12 col-lg-5 m-3 shadow tech-skills ">
-            <p className="text-center h5 pt-2">Tech Skills</p>
-            <hr className="block block-blue block-short" />
+            <div className="col-hr">
+              <p className="text-center h5 pt-2">Tech Skills</p>
+              <hr className="block block-blue block-short block-center" />
+            </div>
             <div className="col">
               <div className="mt-4 mb-1 ">
                 <span>HTML</span>
@@ -259,7 +269,10 @@ function folioBody() {
           <div className="col-lg-1"></div>
 
           <div className="col-sm-12 col-lg-5 m-3 shadow soft-skills ">
-            <h4>Strength</h4>
+            <div className="col-hr">
+              <p className="text-center h5 pt-2">Strength</p>
+              <hr className="block block-blue block-short block-center" />
+            </div>
             <div class="col">
               <div class="svg-item hvr-bob">
                 <svg
@@ -310,8 +323,10 @@ function folioBody() {
 
       {/* Education */}
       <section className="container  mt-3 " id="education">
-        <h4 className="h4 text-center pt-4 ">Education</h4>
-        <hr className="block" />
+        <div className="col-hr">
+          <h4 className="h4 text-center pt-4 ">Education</h4>
+          <hr className="block   block-center" />
+        </div>
         <div className="row">
           <div className="col-sm-1"></div>
           <div className="col-sm-10">
@@ -416,15 +431,132 @@ function folioBody() {
         </div>
       </section>
 
+      <section className="container mt-3 mb-3 contact-section " id="contact">
+        <div className="col-hr">
+          <h4 className="text-center pt-4 h4">Contact me</h4>
+          <hr className="block block-center" />
+        </div>
+        <div className="row">
+          {/* Left contact section */}
+          <div className="col-lg-6">
+            <div className="row">
+              <div className="col-8">
+                <h4 className="pt-4 m-0">Get in touch</h4>
+                <hr className="block block-left block-blue pt-0" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-8">
+                <h5>let's Talk!</h5>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-8">
+                <p>
+                  Don't like forms? Send us{" "}
+                  <a href="mailto:muddasirjaved92@gmail.com">email.</a>{" "}
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-10">
+                <img
+                  src={EmailPic}
+                  alt=""
+                  className="email-pic "
+                  style={{ height: "500px ", width: "" }}
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* message form right side */}
+          <div
+            className="col-lg-6 vertical-align rounded shadow "
+            style={{ backgroundColor: "#bfbfbf" }}
+          >
+            <div className="form-width  rounded" id="contact-form">
+              <form action="">
+                <fieldset>
+                  <legend className="text-center pt-5">
+                    Send me a Message{" "}
+                    {/* <i className="fa fa-smile-o shadow rounded"></i> */}
+                  </legend>
+                  <div className="col  mb-4">
+                    <label htmlFor="fullname">
+                      Name:<span className="asteriks ml-1">*</span>
+                    </label>
+                    <div className="  input-container ">
+                      <i className="fa fa-user-circle form-icon"></i>
+                      <input
+                        type="text"
+                        name="Name"
+                        id="fullname"
+                        className="form-control "
+                        placeholder="e.g. Shari Ali "
+                        // style={{widows:"80%"}}
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col mb-3">
+                    <label htmlFor="emailaddress">
+                      E-mail: <span className="asteriks ml-1">*</span>{" "}
+                    </label>
+                    <div>
+                      <i className="fa fa-envelope form-icon "></i>
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="form-control"
+                        required
+                        placeholder="e.g. m.muddasirjaved@gmail.com"
+                      />
+                    </div>
+                  </div>
+                  <div className="col mb-2 ">
+                    <label htmlFor="messagerequest">
+                      Message <span className="asteriks ml-1">*</span>
+                    </label>
+
+                    {/* <i className="fa fa-envelope form-icon mr-0"></i> */}
+                    <textarea
+                      rows={"5"}
+                      name="message"
+                      id="message"
+                      className="form-control "
+                      placeholder="e.g m. Hello! I need a website......"
+                      required
+                    ></textarea>
+                  </div>
+
+                  <div className="text-center mb-2">
+                    <div class="col ">
+                      <button type="submit" class="btn">
+                        Send Message
+                        <i class="fa fa-paper-plane submt-icon"></i>
+                      </button>
+                    </div>
+                  </div>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+
       {/* Footer */}
+
       <footer
         className="footer"
         id="footer"
-        style={{ backgroundColor: "rgb(157, 103, 211) ", color: "#fff" }}
+        style={{ backgroundColor: "rgb(67 43 91) ", color: "#fff" }}
       >
         <div className="container">
           <div className="row">
-            <div className="col-sm-12 col-md-4 ">
+            <div className="col-sm-12 col-md-4 pt-4 ">
               <h6 className="text-uppercase font-weight-bold step-title pt- ">
                 about
               </h6>
@@ -435,7 +567,7 @@ function folioBody() {
               </p>
             </div>
 
-            <div className="footer-links col-sm-12 col-md-4">
+            <div className="footer-links col-sm-12 col-md-4  ">
               <h6 className="text-uppercase pt-4 font-weight-bold step-title">
                 FIND ME ON SOCIAL MEDIA
               </h6>
@@ -453,7 +585,7 @@ function folioBody() {
                   </a>
                 </li>
                 <li>
-                  <a href="">
+                  <a href="https://www.facebook.com/hafiz.muddasir4/">
                     <i className="fa fa-facebook"></i>
                   </a>
                 </li>
@@ -469,8 +601,12 @@ function folioBody() {
                 </li>
               </ul>
             </div>
-            <div className="col-sm-12 col-md-4">
-              <h1>Hello</h1>
+            <div className="col-sm-12 col-md-4 pt-4 ">
+              <h3>view cv</h3>
+              <p>
+                Need a downloadable format of my CV? Download it here.{" "}
+                <i className="fa fa-download"></i>
+              </p>
             </div>
           </div>
         </div>
